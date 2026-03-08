335f3053fa4ab621b9fb52083d04e219bc8fb2e3
ashish <ashish@ashishs-MacBook-Air.local>
2026-03-08 17:20:17 +0530

    UI: Update background to grid paper style

--- src/components/PaperTexture.tsx
+++ src/components/PaperTexture.tsx
@@ -1,24 +1,16 @@
 export default function PaperTexture() {
   return (
-    <>
+    <div className="fixed inset-0 pointer-events-none z-[-2] bg-white">
       <div 
-        className="fixed inset-0 pointer-events-none z-[-2]"
-        style={{ backgroundColor: '#F5F2EB' }}
+        className="absolute inset-0 opacity-50"
+        style={{ 
+          backgroundImage: `
+            linear-gradient(to right, #818cf8 1px, transparent 1px),
+            linear-gradient(to bottom, #818cf8 1px, transparent 1px)
+          `,
+          backgroundSize: '40px 40px'
+        }}
       />
-      <div className="fixed inset-0 pointer-events-none z-[-1] opacity-[0.4] mix-blend-multiply">
-        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
-          <filter id="paper-noise">
-            <feTurbulence 
-              type="fractalNoise" 
-              baseFrequency="0.04" 
-              numOctaves="4" 
-              stitchTiles="stitch" 
-            />
-            <feColorMatrix type="matrix" values="1 0 0 0 0, 0 1 0 0 0, 0 0 1 0 0, 0 0 0 0.15 0" />
-          </filter>
-          <rect width="100%" height="100%" filter="url(#paper-noise)" />
-        </svg>
-      </div>
-    </>
+    </div>
   );
 }
